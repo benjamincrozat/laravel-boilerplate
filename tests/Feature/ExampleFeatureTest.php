1a1da7ecb0f99df68ef7f40d79adf042b8aab40e
Benjamin Crozat <benjamincrozat@me.com>
2018-05-16 09:36:49 +0200

    Updated TestCase

--- tests/Feature/ExampleFeatureTest.php
+++ tests/Feature/ExampleFeatureTest.php
@@ -2,6 +2,14 @@
 
 class ExampleFeatureTest extends TestCase
 {
+    public function setUp()
+    {
+        parent::setUp();
+        
+        // If your tests hit Redis, you should flush the cache before every test.
+        // cache()->flush();
+    }
+    
     /** @test */
     public function it_works()
     {
