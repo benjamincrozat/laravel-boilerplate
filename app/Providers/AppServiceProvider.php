f3ba73061a842c6e853b5b47a2988440506a9c47
Benjamin Crozat <benjamin.crozat@lagardere-active.com>
2018-07-25 10:54:07 +0200

    Fixed Dusk prod error + better identifiers for poly relationships + clean up

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Carbon;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +14,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Carbon::setLocale(config('app.locale'));
+
+        // Use a clean template for paginations instead of the Bootstrap 4 version.
+        Paginator::defaultSimpleView('pagination::simple-default');
+
+        // Polymorphic relationships shouldn't depend on our app's structure.
+        // Now, instead of seeing `App\User` in the database,
+        // we'll see a proper identifier like `users`.
+        Relation::morphMap([
+            \App\User::class,
+        ]);
     }
 
     /**
@@ -21,6 +34,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if (! $this->app->environment('production')) {
             $this->app->register(\Clockwork\Support\Laravel\ClockworkServiceProvider::class);
+            $this->app->register(\Laravel\Dusk\DuskServiceProvider::class);
         }
     }
 }
