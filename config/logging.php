9a625b0b219f78e43eac05155b9a132084eac2ea
Benjamin Crozat <benjamincrozat@me.com>
2018-10-21 09:57:23 +0200

    Updated config files

--- config/logging.php
+++ config/logging.php
@@ -1,6 +1,7 @@
 <?php
 
 use Monolog\Handler\StreamHandler;
+use Monolog\Handler\SyslogUdpHandler;
 
 return [
     /*
@@ -34,7 +35,7 @@ return [
     'channels' => [
         'stack' => [
             'driver'   => 'stack',
-            'channels' => ['single'],
+            'channels' => ['daily'],
         ],
 
         'single' => [
@@ -47,7 +48,7 @@ return [
             'driver' => 'daily',
             'path'   => storage_path('logs/laravel.log'),
             'level'  => 'debug',
-            'days'   => 7,
+            'days'   => 14,
         ],
 
         'slack' => [
@@ -56,6 +57,16 @@ return [
             'username' => 'Laravel Log',
             'emoji'    => ':boom:',
             'level'    => 'critical',
+        ],
+
+        'papertrail' => [
+            'driver'       => 'monolog',
+            'level'        => 'debug',
+            'handler'      => SyslogUdpHandler::class,
+            'handler_with' => [
+                'host' => env('PAPERTRAIL_URL'),
+                'port' => env('PAPERTRAIL_PORT'),
+            ],
         ],
 
         'stderr' => [
