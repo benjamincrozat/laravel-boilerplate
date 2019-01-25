3d3c5cf4b3d486a57d06de4b73b48bbf9f6fa366
Benjamin Crozat <benjamincrozat@me.com>
2019-01-25 13:12:33 +0100

    Added canonical link tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,12 +3,16 @@
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width,initial-scale=1,viewport-fit=cover">
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        @if (! app()->environment('production'))
+            <meta name="robots" content="noindex, nofollow">
+        @endif
 
         @title($title)
 
         <link rel="stylesheet" href="@mix('/css/app.css')">
+        <link rel="canonical" href="{{ URL::current() }}">
     </head>
     <body class="bg-orange-lightest">
         <div id="app">
